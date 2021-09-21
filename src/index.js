bb0dfb09217e64928fdd080abd77a485192dab4c
KotaMaeyama <90817247+KotaMaeyama@users.noreply.github.com>
2021-09-21 14:28:51 +0900

    セクション4（～27. セクションまとめ） セクション4：モダンJavaScriptの機能に触れる

--- src/index.js
+++ src/index.js
@@ -181,8 +181,38 @@
  * 三項演算子
  */
 // ある操作 ? 条件がtrueの時 : 条件がfalseの時
-// const val1 = 1 > 0 ? 'true' : 'false';
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
 // console.log(val1);
 
 // const num = 1300;
-// num.toLocaleString();
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！！' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります")
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります")
+// }
+
+// || は左側がfalseなら右側を返す、左側がtrueなら左側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す、左側がfalseなら左側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
